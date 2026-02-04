c23d1f4a2c019f8668cbef73f3d56caa094431ba
Nouri-Anouar <nouri.abde.dev@gmail.com>
2026-02-04 22:30:46 +0100

    attempt fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    rollupOptions: {
-      onwarn() {
-        // Ignore all warnings
-        return
-      }
-    }
-   }
 })
